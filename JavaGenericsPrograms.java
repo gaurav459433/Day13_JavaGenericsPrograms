71162cb2961338abfac5053663a2c61c50aa4f2c
gaurav459433 <gaurav459433@gmail.com>
2023-03-07 20:26:30 +0530

    Added: Code For Maximum String Number Among 3 String Numbers

--- JavaGenericsPrograms.java
+++ JavaGenericsPrograms.java
@@ -5,6 +5,7 @@ public class JavaGenericsPrograms {
         System.out.println("Welcome To Java Generics Programs: ");
         System.out.println("The Max Integer Number Among 3 Integer Numbers is: "+maxNumberInteger(93, 26, 43));
         System.out.println("The Max Float Number Among 3 Float Numbers is: "+maxNumberFloat(93.32f, 2698.76f, 437.3f));
+        System.out.println("The Max String Number Among 3 String Number is: "+maxStringNumber("Apple", "Peach", "Banana"));
     }
 
     public static Integer maxNumberInteger(Integer num1, Integer num2, Integer num3)
@@ -22,6 +23,18 @@ public class JavaGenericsPrograms {
     public static Float maxNumberFloat(Float num1, Float num2, Float num3)
     {
         Float max = num1;
+        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+            max = num1;
+        else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+            max = num2;
+        else
+            max = num3;
+        return max;
+    }
+
+    public static String maxStringNumber(String num1, String num2, String num3)
+    {
+        String max = num1;
         if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
             max = num1;
         else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
