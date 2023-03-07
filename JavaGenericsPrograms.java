6b796d7dd0a006dbf48c9c06e6b18256d42d8522
gaurav459433 <gaurav459433@gmail.com>
2023-03-07 20:19:24 +0530

    Added: Code For Maximum Float Number Among 3 Float Numbers

--- JavaGenericsPrograms.java
+++ JavaGenericsPrograms.java
@@ -4,11 +4,24 @@ public class JavaGenericsPrograms {
     public static void main(String[] args) {
         System.out.println("Welcome To Java Generics Programs: ");
         System.out.println("The Max Integer Number Among 3 Integer Numbers is: "+maxNumberInteger(93, 26, 43));
+        System.out.println("The Max Float Number Among 3 Float Numbers is: "+maxNumberFloat(93.32f, 2698.76f, 437.3f));
     }
 
     public static Integer maxNumberInteger(Integer num1, Integer num2, Integer num3)
     {
         Integer max = num1;
+        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+            max = num1;
+        else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+            max = num2;
+        else
+            max = num3;
+        return max;
+    }
+
+    public static Float maxNumberFloat(Float num1, Float num2, Float num3)
+    {
+        Float max = num1;
         if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
             max = num1;
         else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
