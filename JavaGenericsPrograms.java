53ce0888d7dc5df3ec21470da9468f5d5cbfb90d
gaurav459433 <gaurav459433@gmail.com>
2023-03-07 20:10:43 +0530

    Welcome To Java Generics Programs

---
+++ JavaGenericsPrograms.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignmentsrfp259.day13.javagenerics;
+
+public class JavaGenericsPrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java Generics Programs: ");
+    }
+}
